35d17ba1726a72b6ac9a232bbec68caba1cbbdd5
Yanis Guenane <yanis.guenane@enovance.com>
2014-04-03 08:44:00 -0400

    Add packages to be installed on the CI node
    Add a list of packages to be installed on the CI node.
    They are currently three supported ways to install packages.
    
      * System
      * Pip (python)
      * Gem (ruby)

--- manifests/ci.pp
+++ manifests/ci.pp
@@ -18,11 +18,27 @@ class softwarefactory::ci(
   $ldap_manager_password = 'test',
   $ldap_user_base        = 'ou=Users',
   $ldap_user_search      = 'cn={0}',
+  $system_packages       = ['rubygems', 'rake', 'puppet-lint', 'python-pip'],
+  $pip_packages          = ['flake8'],
+  $gem_packages          = ['rspec-puppet', 'serverspec'],
 ) {
 
   include softwarefactory
+
   $ldap_url = "${ldap_protocol}://${ldap_address}"
   $base64_passwd = base64('encode', $ldap_manager_password)
+
+  package {$system_packages :
+    ensure => present,
+  }
+  package {$pip_packages :
+    ensure   => present,
+    provider => 'pip',
+  }
+  package {$gem_packages :
+    ensure   => present,
+    provider => 'gem',
+  }
 
   class {'jenkins' :
     lts                => true,
